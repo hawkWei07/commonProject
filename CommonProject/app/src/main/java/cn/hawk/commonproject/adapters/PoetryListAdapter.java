ae1c58d0387f4e0316ffbcb023383fc98d8ba6be
hawkWei07 <879421299@qq.com>
2017-05-31 09:47:37 +0800

    [Modify] add base adapter for recycler view.

--- CommonProject/app/src/main/java/cn/hawk/commonproject/adapters/PoetryListAdapter.java
+++ CommonProject/app/src/main/java/cn/hawk/commonproject/adapters/PoetryListAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import cn.hawk.commonlib.base.BaseAdapter;
 import cn.hawk.commonlib.interfaces.OnItemClickListener;
 import cn.hawk.commonlib.utils.LogUtils;
 import cn.hawk.commonproject.R;
@@ -18,57 +19,28 @@ import cn.hawk.commonproject.models.PoetryItemBean;
  * Created by kehaowei on 2017/4/11.
  */
 
-public class PoetryListAdapter extends RecyclerView.Adapter<PoetryListAdapter.ViewHolder> {
+public class PoetryListAdapter extends BaseAdapter<PoetryItemBean, PoetryListAdapter.ViewHolder> {
     private static final String TAG = PoetryListAdapter.class.getSimpleName();
-    private Context context;
-    private ArrayList<PoetryItemBean> mData = new ArrayList<>();
     private OnItemClickListener mListener;
 
     public PoetryListAdapter(Context context, ArrayList<PoetryItemBean> mData) {
-        this.context = context;
-        this.mData = mData;
+        super(context, mData);
     }
+
 
     public void setmListener(OnItemClickListener mListener) {
         this.mListener = mListener;
     }
 
-    public void refresh(ArrayList<PoetryItemBean> data) {
-        if (null != data && data.size() > 0)
-            mData = data;
-        else
-            mData.clear();
-        notifyDataSetChanged();
-    }
-
-    public void addData(int position, PoetryItemBean data) {
-        mData.add(position, data);
-        notifyItemInserted(position);
-    }
-
-    public void removeData(int position) {
-        if (position < 0 || position >= mData.size())
-            return;
-        mData.remove(position);
-        notifyItemRemoved(position);
-    }
-
-    public void updateData(int position, PoetryItemBean data) {
-        if (position < 0 || position >= mData.size())
-            return;
-        mData.set(position, data);
-        notifyItemChanged(position);
-    }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    protected ViewHolder getCustomHolder(ViewGroup parent, int viewType) {
         ViewHolder holder = new ViewHolder(LayoutInflater.from(context).inflate(R.layout.view_item_poetry_info, parent, false));
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        PoetryItemBean item = mData.get(position);
+    protected void setView(PoetryItemBean item, final ViewHolder holder) {
         holder.title.setText(item.getTitle());
         holder.time.setText(item.getTime());
         holder.author.setText(item.getAuthor());
@@ -95,10 +67,6 @@ public class PoetryListAdapter extends RecyclerView.Adapter<PoetryListAdapter.Vi
         });
     }
 
-    @Override
-    public int getItemCount() {
-        return mData.size();
-    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView title;
